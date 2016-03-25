1a5b9a86f33ddcf9fd65330399484795890af27f
landlessness <brian@landlessness.net>
2016-03-25 15:38:45 -0400

    start stop the flow of mock data

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ var Photocell = module.exports = function(opts) {
   Device.call(this);
   this.opts = opts || {};
   this.intensity = 0;
-  this.increment = this.opts['increment'] || 15;
+  this._increment = this.opts['increment'] || 15;
+  this._timeOut = null;
+  this._counter = 0;
 };
 util.inherits(Photocell, Device);
 
@@ -19,12 +21,37 @@ Photocell.prototype.init = function(config) {
   config
     .name(name)
     .type('photocell')
+    .state('ready')
+    .when('ready', {allow: ['make-not-ready']})
+    .when('not-ready', {allow: ['make-ready']})
+    .map('make-not-ready', this.makeNotReady)
+    .map('make-ready', this.makeReady)
     .monitor('intensity');
 
-  var self = this;
-  var counter = 0;
-  setInterval(function() {
-    self.intensity = Math.sin(degToRad(counter)) + 1.0;
-    counter += self.increment;
-  }, 100);
+  this._startMockData();
 };
+
+Photocell.prototype.makeReady = function(cb) {
+  this.state = 'ready';
+  this._startMockData();
+  cb();
+}
+
+Photocell.prototype.makeNotReady = function(cb) {
+  this.state = 'not-ready'
+  this._stopMockData();
+  cb();
+}
+
+Photocell.prototype._startMockData = function(cb) {
+  var self = this;
+  this._timeOut = setInterval(function() {
+    self.intensity = Math.sin(degToRad(self._counter)) + 1.0;
+    self._counter += self._increment;
+  }, 100);
+}
+
+Photocell.prototype._stopMockData = function(cb) {
+  clearTimeout(this._timeOut);
+}
+
